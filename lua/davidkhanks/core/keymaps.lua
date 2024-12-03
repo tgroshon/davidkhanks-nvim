555024fb38b7bdd0a2b78f0622498ef66e3910a3
Tommy Groshong <tgroshong@notablehealth.com>
2024-12-03 12:08:25 -0500

    Better quit

--- lua/davidkhanks/core/keymaps.lua
+++ lua/davidkhanks/core/keymaps.lua
@@ -29,9 +29,8 @@ vim.keymap.set("n", "<leader>gs", ":Neogit<CR>", { desc = "Open neogit status bu
 -- Buffer commands
 vim.keymap.set("n", "<leader>bd", ":bp|sp|bn|bd<CR>", { desc = "Kill the current buffer" }) -- Kill the current buffer and switch the next one
 vim.keymap.set("n", "<leader>bD", ":w|%bd|e#<CR>", { desc = "Kill all other buffers" }) -- save this buffer then, kill all other buffers
-vim.keymap.set("n", "<leader>qs", ":w|%bd|e#<CR>:q!<CR>", { desc = "Save buffer and quit" }) -- save this buffer then kill all other buffers and force quit
-vim.keymap.set("n", "<leader>qq", ":%bd|e#<CR>:q!<CR>", { desc = "Quit; no save" }) -- kill all other buffers and force quit
-vim.keymap.set("n", "<leader>qQ", ":%bd!|e#<CR>:q!<CR>", { desc = "HARD Quit; no save" }) -- kill all other buffers and force quit
+vim.keymap.set("n", "<leader>qs", ":w|%bd|e#<CR>:q!<CR>", { desc = "Save and quit" }) -- save this buffer then kill all other buffers and force quit
+vim.keymap.set("n", "<leader>qq", ":qall!<CR>", { desc = "Quit" }) -- force quit
 
 -- File commands
 vim.keymap.set("n", "<leader>fs", ":write<CR>", { desc = "Save buffer" })
