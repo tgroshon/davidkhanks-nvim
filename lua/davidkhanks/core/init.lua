c48bdc6ac97d77b25fc47650f2efa765c3c95a0b
Tommy Groshong <tom@groshong.software>
2025-09-19 11:39:10 -0400

    Add Expert - Elixir Language Server

--- lua/davidkhanks/core/init.lua
+++ lua/davidkhanks/core/init.lua
@@ -1,2 +1,11 @@
 require("davidkhanks.core.options")
 require("davidkhanks.core.keymaps")
+
+-- Setup Expert - Elixir Language Server
+vim.lsp.config("expert", {
+	cmd = { "expert" },
+	root_markers = { "mix.exs", ".git" },
+	filetypes = { "elixir", "eelixir", "heex" },
+})
+
+vim.lsp.enable("expert")
