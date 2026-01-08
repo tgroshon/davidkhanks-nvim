7d27242e26bf4a9f4a4c7998a5316111a83cf63c
Tom Groshong <tgroshon@pm.me>
2026-01-08 16:35:27 -0500

    Drop old elixir-tools for new expert and manual treesitter

--- lua/davidkhanks/core/init.lua
+++ lua/davidkhanks/core/init.lua
@@ -3,9 +3,17 @@ require("davidkhanks.core.keymaps")
 
 -- Setup Expert - Elixir Language Server
 vim.lsp.config("expert", {
-	cmd = { "expert" },
+	cmd = { "expert", "--stdio" },
 	root_markers = { "mix.exs", ".git" },
 	filetypes = { "elixir", "eelixir", "heex" },
 })
 
 vim.lsp.enable("expert")
+
+-- Ensure Tree-sitter highlighting is enabled for Elixir files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "elixir", "eelixir", "heex" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
