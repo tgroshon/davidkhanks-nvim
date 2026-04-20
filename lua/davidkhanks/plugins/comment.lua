1c0f2fa3a119977f7cf1279da3b9905e7b629f3e
Tom Groshong <tgroshon@pm.me>
2026-04-20 11:56:58 -0400

    Fixup tscomments broken deps

--- lua/davidkhanks/plugins/comment.lua
+++ lua/davidkhanks/plugins/comment.lua
@@ -1,20 +1,16 @@
 return {
-	"numToStr/Comment.nvim",
-	commit = "e30b7f2008e52442154b66f7c519bfd2f1e32acb",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "JoosepAlviste/nvim-ts-context-commentstring", commit = "9c74db656c3d0b1c4392fc89a016b1910539e7c0" },
+	{
+		"numToStr/Comment.nvim",
+		commit = "e30b7f2008e52442154b66f7c519bfd2f1e32acb",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("Comment").setup()
+		end,
 	},
-	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
-
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
-		comment.setup({
-			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
-		})
-	end,
+	{
+		"folke/ts-comments.nvim",
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+		opts = {},
+	},
 }
